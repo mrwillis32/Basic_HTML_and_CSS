b724ef6de9e9600b6b3cd2845da7c55a914ef3bd
mrwillis32 <83623388+mrwillis32@users.noreply.github.com>
2021-10-10 11:43:14 -0400

    stylesheet

---
+++ Hello_World.js
@@ -0,0 +1,8 @@
+var greeting = 'Hello, World!' ;
+console.log(greeting);
+
+var greeting2= 'Welcome to Node.js';
+console.log(gretting2);
+
+var sentence1 ='Learning how to debug code with the debugger';
+console.log(sentenceOne );
